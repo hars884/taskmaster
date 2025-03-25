fa373a1034bb0ae31673d0f30b27ff0b00e54b34
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-03-25 16:22:06 +0530

    connected with database

--- app.py
+++ app.py
@@ -1,66 +1,39 @@
-from flask import Flask,redirect,render_template,request,session,url_for
-# from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,redirect,render_template,request,url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:harshu8564@localhost/TASK'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:roobika@localhost:5432/task'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-# class userlo(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     usernam = db.Column(db.String(80), nullable=False, unique=True)
-#     password = db.Column(db.String(120), nullable=False)
+class userlo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usernam = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
 
-#     def __repr__(self):
-#         return f"<User {self.usernam}>"
+    def __repr__(self):
+        return f"<User {self.usernam}>"
 
-# class tasks(db.Model):
-#     id=db.Column(db.Integer, primary_key=True)
-#     task=db.Column(db.String(100), nullable=False)
-#     date=db.Column(db.Date, nullable=False)
-#     remark=db.Column(db.String(100), nullable=False)
-#     status=db.Column(db.String(10), nullable=False)
+class tasks(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    task=db.Column(db.String(100), nullable=False)
+    date=db.Column(db.Date, nullable=False)
+    remark=db.Column(db.String(100), nullable=False)
+    status=db.Column(db.String(10), nullable=False)
 
-#     def __repr__(self):
-#         return f"<User {self.id}>"
+    def __repr__(self):
+        return f"<User {self.id}>"
 
-# class usrdetail(db.Model):
-#     id=db.Column(db.Integer, primary_key=True)
-#     name=db.Column(db.String(30), nullable=False)
-#     occubation=db.Column(db.String(30), nullable=False)
-#     email=db.Column(db.String(100), nullable=False)
+class usrdetail(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(30), nullable=False)
+    occubation=db.Column(db.String(30), nullable=False)
+    email=db.Column(db.String(100), nullable=False)
 
-#     def __repr__(self):
-#         return f"<User {self.name}>"
-
-# @app.route('/')
-# def home():
-#     render_template(url_for("login"))
-
-# @app.route('login',methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         usr=userlo.query.filter_by(usernam=username)
-#         if usr and usr.password == password:
-#             session['id'] = usr.id
-#             return redirect(url_for())
-#         else:
-#             error = "Please enter correct username and password."
-#             return render_template('login.html', error=error)
-#     return render_template("login.html")
-
-# @app.route('home',method=['GET','POST'])
-# def home():
-#     taskb=tasks.query.filter_by(id=session['id'])
-#     return render_template("home.html",taskf=taskb) 
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()
-#     app.run(debug=True)
+    def __repr__(self):
+        return f"<User {self.name}>"
 
 @app.route('/')
 def login():
@@ -76,9 +49,22 @@ def login_post():
     else:
         return "Invalid credentials, try again!"
 
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    username = request.form.get('username')  # Ensure the form has a username field
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirm_password')
+
+    if password != confirm_password:
+        return "Passwords do not match, try again!"
+    return redirect(url_for('home'))
+
+
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    usr=tasks.query.all()
+    return render_template('home.html',usr=usr)
 
 if __name__ == '__main__':
     app.run(debug=True)
